418a837dbc8da5fdedd009166b45c289544e4cbb
axlecho <axlecho@gmail.com>
2018-05-31 19:23:51 +0800

    [feture] save to database

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var sqlite3 = require('sqlite3');
+var async = require("async");
+
 var DATABASE_STRING = "data.db";
 
-http.get("http://apiv3.yangkeduo.com/search?page=1&size=10&sort=_sales&q=%E7%8E%A9%E5%85%B7pdduid=5799600966", function(res) {
+var db;
+var info;
+
+http.get("http://apiv3.yangkeduo.com/search?page=1&size=500&sort=_sales&q=%E7%8E%A9%E5%85%B7pdduid=5799600966", function(res) {
 	console.log("Got response: " + res.statusCode);
 	
 	var data = [];
@@ -12,28 +17,47 @@ http.get("http://apiv3.yangkeduo.com/search?page=1&size=10&sort=_sales&q=%E7%8E%
 	
 	res.on('end', function() {
 		var body = JSON.parse(data.join(''));
-		console.log(body.items);
+		// console.log(body.items);
+		info = body.items;
+		init();
 	});
 }).on('error', function(e) {
 	console.log("Got error: " + e.message);
 });
 
 
-init();
 
 
-function init(info) {
-	var db = new sqlite3.Database(DATABASE_STRING,function() {  
-		db.run("create table good(id varchar(15))",function(){})
-		save(db,info);
+
+function init() {
+	db = new sqlite3.Database(DATABASE_STRING,function() {  
+		db.run("create table goods (goods_id INT PRIMARY KEY NOT NULL,\
+			goods_name TEXT NOT NULL,\
+			sales INT NOT NULL\)",function(){
+				save();
+			});
 	});
 };
 
-function save(db,info) {
-	
+function save() {
+	async.mapLimit(info, 20, async function(item) {
+		const id = await saveItem(item)
+		return id;
+	}, (err, results) => {
+		if (err) throw err
+		// results is now an array of the response bodies
+		// console.log(results)
+	});
 };
 
 
-function saveItem(db,item) {
+function saveItem(item) {
+	const INSERT_STRING = 'INSERT INTO goods \
+		(goods_id, goods_name, sales) \
+		VALUES (' + item.goods_id + ',"' + item.goods_name + '",' + item.sales + ')';
+	// console.log(INSERT_STRING);
+	db.run(INSERT_STRING);
 	
+	console.log(item.goods_name);
+	return item.goods_id;
 }
